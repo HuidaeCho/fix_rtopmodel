4d8de275259e47a91b337f49ac70531e909de9f3
Huidae Cho <grass4u@gmail.com>
2025-02-26 08:42:05 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# fix_rtopmodel
+
+This small program fixes [r.topmodel](https://grass.osgeo.org/grass-stable/manuals/r.topmodel.html) input files to work around [a bug in r.topmodel](https://github.com/OSGeo/grass/pull/5218).
---
+++ fix_rtopmodel.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    if (argc < 2) {
+        printf
+            ("This program fixes r.topmodel input files by converting UNIX files to DOS file format and removing newline at the end of a file.\n\n");
+        printf("Usage: fix_rtopmodel file...\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    for (i = 1; i < argc; i++) {
+        FILE *infp, *outfp;
+        char outfile[1024], buf[1024];
+        int first = 1;
+
+        if (!(infp = fopen(argv[i], "r"))) {
+            printf("Failed to open %s\n", argv[i]);
+            exit(EXIT_FAILURE);
+        }
+
+        sprintf(outfile, "%s.out", argv[i]);
+
+        if (!(outfp = fopen(outfile, "w"))) {
+            printf("Failed to create %s\n", outfile);
+            exit(EXIT_FAILURE);
+        }
+
+        while (!feof(infp)) {
+            if (first)
+                first = 0;
+            else
+                fprintf(outfp, "\n");
+            fscanf(infp, " %[^\r\n] ", buf);
+            fprintf(outfp, "%s", buf);
+        }
+
+        fclose(infp);
+        fclose(outfp);
+    }
+}
